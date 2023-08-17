1fa9ca216b4256a114cef23ae336db333a022bb9
mjaumi <mjaumi2864@gmail.com>
2023-08-17 22:52:13 +0600

    no country found message fixed

--- src/components/Countries/CountriesContainer.tsx
+++ src/components/Countries/CountriesContainer.tsx
@@ -24,7 +24,7 @@ const CountriesContainer = ({countries}: {countries: Country[]}) => {
     return (
         <>
             {
-                countries.filter(searchCountry).length ?
+                countries.filter(filterCountries).filter(searchCountry).length ?
                     <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 xl:gap-24'>
                         {
                             countries
