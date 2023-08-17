38f75f5cd73e92f5f3c2cb5e3d2f028415987dd5
mjaumi <mjaumi2864@gmail.com>
2023-08-17 20:43:52 +0600

    navigate to border country details page done

--- src/components/BorderCountries/BorderCountriesButton.tsx
+++ src/components/BorderCountries/BorderCountriesButton.tsx
@@ -1,12 +1,23 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const BorderCountriesButton = ({borderCountry}: {borderCountry: Partial<Country>}) => {
     // destructuring the border country component here
     const {name, cca3} = borderCountry;
 
+    // integration of next hooks here
+    const router = useRouter();
+
+    // this function is navigating user to border country details page
+    const navigateToBorderCountry = (countryCode: string | undefined) => {
+        router.push(`/country-details/${countryCode}`);
+    }
+
     // rendering border countries button component here
     return (
-        <button className='bg-white dark:bg-REST-dark-blue py-1 px-5 shadow-REST-shadow ml-2 rounded text-sm hover:opacity-60 duration-300'>
+        <button onClick={() => navigateToBorderCountry(cca3)} className='bg-white dark:bg-REST-dark-blue py-1 px-5 shadow-REST-shadow ml-2 rounded text-sm hover:opacity-60 duration-300'>
             {name?.common}
         </button>
     );
